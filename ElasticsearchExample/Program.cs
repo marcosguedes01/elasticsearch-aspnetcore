49e74ac6ac750281c5e7973c2d56921b0bc2f3b8
Marcos Guedes <mguedes@growin.pt>
2019-07-16 17:20:48 +0100

    Arquivos iniciais. Criando indice no elasticsearch

---
+++ ElasticsearchExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using Nest;
+using System;
+
+namespace ElasticsearchExample
+{
+    class Program
+    {
+        public static Uri node { get; set; }
+        public static ConnectionSettings settings { get; set; }
+        public static ElasticClient client { get; set; }
+
+        static void Main(string[] args)
+        {
+            node = new Uri("http://localhost:9200");
+            settings = new ConnectionSettings(node, defaultIndex: "my_blog");
+            client = new ElasticClient(settings);
+
+            var indexSettings = new IndexSettings();
+            indexSettings.NumberOfReplicas = 1;
+            indexSettings.NumberOfShards = 1;
+
+            //client.CreateIndex(c => c
+            //    .Index("my_blog")
+            //    .InitializeUsing(indexSettings)
+            //    .AddMapping<Post>(m => m.MapFromAttributes()));
+
+            InsertData();
+        }
+
+        public static void InsertData()
+        {
+            var newBlogPost = new Post
+            {
+                UserId = 1,
+                PostDate = DateTime.Now,
+                PostText = "This is a blog post from NEST!"
+            };
+
+            client.Index(newBlogPost);
+        }
+    }
+}
